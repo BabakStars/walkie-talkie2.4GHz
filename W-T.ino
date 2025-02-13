25c7fb9243b5311afd0bf358439c5192ecc572f8
Babak <111429093+BabakStars@users.noreply.github.com>
2025-02-13 22:06:48 +0330

    Update W-T.ino

--- W-T.ino
+++ W-T.ino
@@ -1,34 +1,86 @@
 #include <RF24.h>
 #include <SPI.h>
 #include <RF24Audio.h>
+#include <Wire.h>
+#include <TM1637.h>
 #include "printf.h"
 
 RF24 radio(7,8);    // Set radio up using pins 7 (CE) 8 (CS)
 RF24Audio rfAudio(radio,0); 
 
-int talkButton = 3;
+
+int Channel = 0;
+
+TM1637 tm(A5, A4);
 
 void setup() {      
   Serial.begin(115200);
   
+  tm.begin();
+  tm.setBrightnessPercent(90);
+  tm.display("----");
+  
+  pinMode(4,INPUT_PULLUP);
+  pinMode(5,INPUT_PULLUP);
+  pinMode(6,INPUT_PULLUP);
+  
+  
   printf_begin();
   radio.begin();
-  radio.printDetails();
   rfAudio.begin();
 
-  pinMode(talkButton, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(talkButton), talk, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(3), talk, CHANGE);
   rfAudio.receive();
+  
 }
 
 
 void talk()
 {
-  if (!digitalRead(talkButton)) rfAudio.transmit();
-  else rfAudio.receive();
+  if(digitalRead(4) == LOW)
+  {
+    delay(50);
+    Channel++;
+    if(Channel > 125)
+    {
+      Channel = 125;
+    }
+    radio.setChannel(Channel);
+    dis();
+    
+  }
+  if(digitalRead(5) == LOW)
+  {
+    delay(50);
+    Channel--;
+    if(Channel < 0)
+    {
+      Channel = 0;
+    } 
+    radio.setChannel(Channel);
+    dis();
+  }
+  
+  
+  if (!digitalRead(6)) 
+  { 
+    rfAudio.transmit();
+  }
+  else 
+  {
+    rfAudio.receive();
+  }
+
 }
 
 void loop()
 {
+  
+}
+void dis()
+{
+   tm.display("    ");
+   tm.display(Channel);
 }
